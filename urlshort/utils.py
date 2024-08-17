7c8f04957dff6e4e8f25316231f9b38ce3de0428
sid <35936587+siddharth-shringarpure@users.noreply.github.com>
2024-08-17 22:20:58 +0100

    🐛 Fix short code generation with dynamic length adjustment

--- urlshort/utils.py
+++ urlshort/utils.py
@@ -6,8 +6,15 @@ from .models import ShortenedURL
 def generate_short_code(length: int = 6) -> str:
     valid_chars: str = string.ascii_letters + string.digits
 
-    while True:
-        new_code: str = "".join(random.choices(valid_chars, k=length))
+    n: int = len(valid_chars)
+    remaining_attempts: int = n ** length  # Maximum number of codes possible with given length
+    while remaining_attempts > 0:
+        code: str = "".join(random.choices(valid_chars, k=length))
 
-        if not ShortenedURL.query.filter_by(code=new_code).first():
-            return new_code
+        if not ShortenedURL.query.filter_by(code=code).first():
+            return code
+
+        remaining_attempts -= 1
+
+    # Try again with a longer code
+    return generate_short_code(length + 1)
