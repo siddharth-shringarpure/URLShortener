eba2a2fea27929ca0d7ae2c3f340bb0c10c3bcf8
sid <35936587+siddharth-shringarpure@users.noreply.github.com>
2024-08-17 10:50:54 +0100

    🔄 Migrated database to Postgres for Vercel compatibility

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -11,22 +11,13 @@ def create_app(test_config=None):
     my_app = Flask(__name__)
     my_app.secret_key = os.getenv("SECRET_KEY")
 
-    # Ensure the directory exists
-    db_dir = os.path.join(os.getcwd(), "urlshort")
-    if not os.path.exists(db_dir):
-        os.makedirs(db_dir)
+    # Database configuration
 
-    db_path = os.path.join(db_dir, 'db.sqlite3')
+    my_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
-    my_app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
     my_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    if not os.path.exists(db_path):
-        open(db_path, 'a').close()  # Create the file if it doesn't exist
-    os.chmod(db_path, stat.S_IRUSR | stat.S_IWUSR)  # Ensure owner has read and write permissions
-
     db.init_app(my_app)
-
     my_app.register_blueprint(bp)
 
     with my_app.app_context():  # Needed to create the tables
@@ -36,7 +27,7 @@ def create_app(test_config=None):
         except Exception as e:
             print(f"Error creating tables: {e}")
         print(f"Current working directory: {os.getcwd()}")
-        print(f"Database file path: {os.path.join(os.getcwd(), 'urlshort/db.sqlite3')}")
+        print(f"Database URI: {my_app.config['SQLALCHEMY_DATABASE_URI']}")
 
     return my_app
 
